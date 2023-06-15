ed005a6fa665844d122ba44a4128d5993076dc22
Abraham10000 <hei.abraham.2@gmail.com>
2023-06-15 10:54:42 +0300

    feat : creating local storage hook done

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,18 @@
-const useLocalStorage = () => {
+import { useState, useEffect } from 'react';
 
-}
+const useLocalStorage = (key : string, initialValue : any) => {
+  const [value, setValue] = useState(() => {
+    //Verify if the key is already in the local storage
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
 
-export {
-  useLocalStorage
-}
+  useEffect(() => {
+    // Update the value in the local storage everytime there is a current changement of the value
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
